9d3fa54981dfd554394f6a3bf3916bd48fee5900
markolazic01 <markolazic01@gmail.com>
2024-02-19 22:00:50 +0100

    config: DragonswapStakerBoosted wip

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('solidity-coverage');
+require('hardhat-gas-reporter');
+require('hardhat-contract-sizer');
+
+const accounts = process.env.PK ? [process.env.PK] : [];
+const timeout = 50000;
+
+module.exports = {
+  networks: {
+    mainnet: {
+      url: 'to be added',
+      chainId: 0,
+      timeout,
+      accounts,
+    },
+    testnet: {
+      url: "https://evm-rpc.arctic-1.seinetwork.io",
+      chainId: 713715,
+      timeout,
+      accounts,
+    },
+    local: {
+      url: 'http://localhost:8545',
+    },
+    hardhat: {
+      evmVersion: 'paris',
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: true,
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  solidity: {
+    version: '0.8.20',
+    settings: {
+      evmVersion: 'paris',
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+};
