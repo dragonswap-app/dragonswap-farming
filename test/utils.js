3d6102b5079e78dfcb3415c29a8ab36d4aedc3c7
markolazic01 <markolazic01@gmail.com>
2024-02-27 11:51:01 +0100

    :memo: chore: Add tests

---
+++ test/utils.js
@@ -0,0 +1,20 @@
+const { ethers } = require('hardhat');
+
+const blockTimestamp = async () => {
+  return (await ethers.provider.getBlock('latest')).timestamp;
+};
+
+const mineBlock = async () => {
+    await network.provider.send("evm_mine");
+}
+
+const increaseTime = async (time) => {
+    await hre.network.provider.send("evm_increaseTime", [time]);
+    await mineBlock();
+}
+
+module.exports = {
+  blockTimestamp,
+  mineBlock,
+  increaseTime
+};
