da7b442ee7fd50ded7faf62e26f09ccf0ffea45d
milanmitic <miticm97@gmail.com>
2024-02-28 15:14:55 +0100

    :memo: chore: Testing for classic and boosted farm

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -4,7 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor() ERC20("Token", "TKN") {
+    uint8 private tokenDecimals;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
         _mint(msg.sender, 100_000_000 * 1e18);
+        tokenDecimals = _decimals;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return tokenDecimals;
     }
 }
